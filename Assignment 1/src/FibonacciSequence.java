8c1d87c834526c907a33f1765cf4bc435c7c9197
Gary M Montero <117549519+garyMitchellM@users.noreply.github.com>
2025-05-08 13:10:26 -0400

    Javadoc comments
    Include Javadoc comments in the FibonacciSequence class

--- Assignment 1/src/FibonacciSequence.java
+++ Assignment 1/src/FibonacciSequence.java
@@ -1,6 +1,12 @@
 
 public class FibonacciSequence {
 
+    /***
+     * This method includes the logic for calculating the nth number of the Fibonacci sequence
+     * by using recursion
+     * @param n
+     * @return "the result the method gives for n-1" added to "the result the method gives for n-2"
+     */
     public static int Fibonacci(int n) {
 
         if (n <= 1) {
